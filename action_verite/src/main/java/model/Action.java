d829e2da314e1088dfae68ca4a9be3649cd5d63f
YassinAA <Yassin_AA@hotmail.fr>
2020-02-10 14:42:48 +0100

    Ajout classe Verite && Action

---
+++ action_verite/src/main/java/model/Action.java
@@ -0,0 +1,73 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Action", uniqueConstraints = {@UniqueConstraint(columnNames = "action")})
+public class Action {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    @Column(name = "user_id")
+    private Integer id;
+
+    private String action;
+
+    private Integer Level;
+
+    private Boolean isActive;
+
+    public Action() {
+    }
+
+    public Action(String action, Integer level, Boolean isActive) {
+        this.action = action;
+        Level = level;
+        this.isActive = isActive;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public Integer getLevel() {
+        return Level;
+    }
+
+    public void setLevel(Integer level) {
+        Level = level;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Action{" +
+                "id=" + id +
+                ", action='" + action + '\'' +
+                ", Level=" + Level +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
---
+++ action_verite/src/main/java/model/Verite.java
@@ -0,0 +1,73 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Verite", uniqueConstraints = {@UniqueConstraint(columnNames = "verite")})
+public class Verite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    @Column(name = "user_id")
+    private Integer id;
+
+    private String verite;
+
+    private Integer Level;
+
+    private Boolean isActive;
+
+    public Verite() {
+    }
+
+    public Verite(String verite, Integer level, Boolean isActive) {
+        this.verite = verite;
+        Level = level;
+        this.isActive = isActive;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getVerite() {
+        return verite;
+    }
+
+    public void setVerite(String verite) {
+        this.verite = verite;
+    }
+
+    public Integer getLevel() {
+        return Level;
+    }
+
+    public void setLevel(Integer level) {
+        Level = level;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Verite{" +
+                "id=" + id +
+                ", verite='" + verite + '\'' +
+                ", Level=" + Level +
+                ", isActive=" + isActive +
+                '}';
+    }
+}
