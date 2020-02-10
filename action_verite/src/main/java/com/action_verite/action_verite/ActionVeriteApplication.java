eb4181149e281e67fc6073a97419581038297ca6
YassinAA <32573248+YassinAA@users.noreply.github.com>
2020-02-10 14:10:48 +0100

    Initialisation de l'API

---
+++ action_verite/src/main/java/com/action_verite/action_verite/ActionVeriteApplication.java
@@ -0,0 +1,13 @@
+package com.action_verite.action_verite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionVeriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionVeriteApplication.class, args);
+	}
+
+}
---
+++ action_verite/src/test/java/com/action_verite/action_verite/ActionVeriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.action_verite.action_verite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionVeriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
